6431eb8a454573b71152f0536c049637706d4598
Techillionaire <techillionaire@gmail.com>
2024-03-07 13:47:11 +0100

    review app's initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Home from './screens/Home'
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const getFonts = ()  => Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf")
+})
+
+
+const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+
+  if(fontsLoaded){
+    return (
+      <>
+       <Home />
+      </>
+     )
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={()=>setFontsLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    )
+  }
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
---
+++ screens/ReviewDetails.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { globalStyles } from '../styles/global'
+
+const ReviewDetails = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>ReviewDetails</Text>
+    </View>
+  )
+}
+
+export default ReviewDetails
+
+const styles = StyleSheet.create({})
